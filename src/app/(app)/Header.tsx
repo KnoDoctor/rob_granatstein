5885bba4ede19d7e815260fc8ca22d8f23b807aa
Jeff Barfield <barfieldjt@gmail.com>
2024-08-05 11:42:26 -0400

    tweaked key messages3

--- src/app/(app)/Header.tsx
+++ src/app/(app)/Header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <header className="flex gap-2 py-5 px-5 bg-slate-50 text-white items-center">
-      <MessageSquareCodeIcon className="text-red-600" />
+      {/* <MessageSquareCodeIcon className="text-red-600" /> */}
       <Link href="/" className="text-xl font-bold">
         <h1 className="text-red-600">Rob Granatstein</h1>
       </Link>
